e5d89615823f28612cdd11385154be0e6c8e2fb6
Munir Safi <munir.safi1@gmail.com>
2020-11-14 15:13:42 -0500

    feat: starting change-password

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,6 +1,13 @@
+from django.views import View
+from django.http import JsonResponse
+
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import AuthUserObtainPairSerializer
 
 class AuthUserObtainPairView(TokenObtainPairView):
     serializer_class = AuthUserObtainPairSerializer
+
+def change_password(request):
+    print(request.headers['Authorization'])
+    return JsonResponse({ 'hello': 'world' })
--- backend/sharedpathmap/urls.py
+++ backend/sharedpathmap/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from authentication.views import AuthUserObtainPairView
+from authentication.views import AuthUserObtainPairView, change_password
 from landzones.views import LandZoneView
 
 from rest_framework import routers
@@ -12,8 +12,9 @@ router.register(r'landzones', LandZoneView, basename='landzones')
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/token/', AuthUserObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/change-password', change_password, name='change_password'),
+    path('auth/token/', AuthUserObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
