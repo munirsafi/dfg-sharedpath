ea6644cdb9b0d004dde1fb1aeb14bfc8dc7146e3
fatimanadeem <fatima.nadeem@mail.utoronto.ca>
2020-11-14 17:01:17 -0500

    Login Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import L from 'leaflet';
-
+import Login from './Login'
 import './App.scss';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
-            </header>
+            <Login></Login>
         </div>
     );
 }
