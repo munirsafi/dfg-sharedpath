5a9204497283641c8fedff078bd34b28a9cd4103
Munir Safi <munir.safi1@gmail.com>
2020-11-15 02:04:54 -0500

    feat: starting profile pages

---
+++ frontend/src/app/route-profile/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Profile.scss';
+
+export default function Profile() {
+
+    return (
+        <div className="profile-page">
+            Profile page
+        </div>
+    );
+}
---
+++ frontend/src/app/route-profile/change-password/ChangePassword.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './ChangePassword.scss';
+
+export default function ChangePassword() {
+
+    return (
+        <div className="change-password">
+            Change Password page
+        </div>
+    );
+}
