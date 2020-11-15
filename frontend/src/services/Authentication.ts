6252b04e40c89f2264ded30752ea41260388569f
Munir Safi <munir.safi1@gmail.com>
2020-11-15 10:30:45 -0500

    feat: adding refresh token logic

--- frontend/src/services/Authentication.ts
+++ frontend/src/services/Authentication.ts
@@ -1,28 +1,37 @@
-import axios from "axios";
-import { AxiosResponse } from "axios";
+import axios from 'axios';
+import { AxiosResponse } from 'axios';
 
-import { IHeader } from "../interfaces/header";
-import { Token } from "../interfaces/token";
+import { IHeader } from '../interfaces/header';
+import { Token } from '../interfaces/token';
 
-const API_URL: string = "http://localhost:8000";
+const API_URL: string = 'http://localhost:8000';
 
 /**
  * @summary     Given a user is logged in and their active refresh token is valid,
  *              request a new refresh token
  * 
- * @author      
+ * @author      Xunkai Chen, Munir Safi
  * @since       2020-11-14
  */
 async function refreshToken() : Promise<void> {
     const options = {
         headers: generateHeaders(true)
+    };
+
+    const data = {
+        'refresh': localStorage.getItem('refresh_token')
+    };
+
+    try {
+        const response: AxiosResponse<Token> = await axios.post(`${API_URL}/refresh`, data, options);
+        if (response.data.access) {
+            localStorage.setItem('access_token', response.data.access)
+        }
+    } catch(err) {
+        if (localStorage.getItem('DEBUG') === '*') {
+            console.error('An error occurred when refreshing access token: ', err);
+        }
     }
-    // const response: AxiosResponse<Token> = await axios.post(`${API_URL}/refresh`, data, options);
-    // if (response.data.access) {
-    //     const token: Token = JSON.parse('10');
-    //     token.access = response.data.access;
-    //     localStorage.setItem("token", JSON.stringify(token))
-    // }
 }
 
 /**
@@ -31,6 +40,7 @@ async function refreshToken() : Promise<void> {
  * @author      Munir Safi
  * @since       2020-11-15
  * @param       authNeeded Indicate if this request needs authorization headers
+ * @returns     HTTP header object
  */
 function generateHeaders(authNeeded?: boolean) : IHeader {
     const headers: IHeader = {
@@ -56,6 +66,7 @@ const Authentication = {
      * @author      Xuankai Chen, Munir Safi
      * @param       email User's email address
      * @param       password User's plaintext password
+     * @returns     True if user login attempt was successful, false if not
      */
     login: async (email: string, password: string): Promise<boolean> => {
         const data = {
@@ -70,8 +81,8 @@ const Authentication = {
         try {
             const response: AxiosResponse<Token> = await axios.post(`${API_URL}/auth/token/`, data, options);
             if (response.data.access && response.data.refresh) {
-                localStorage.setItem("access_token", response.data.access);
-                localStorage.setItem("refresh_token", response.data.refresh);
+                localStorage.setItem('access_token', response.data.access);
+                localStorage.setItem('refresh_token', response.data.refresh);
 
                 return true
             }
